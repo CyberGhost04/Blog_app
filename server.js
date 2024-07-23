079bda0a50e62c969a03fbc45e726926be227589
CyberGhost04 <sidhantbahuguna@gmail.com>
2024-07-23 22:41:39 +0530

    initial

--- server.js
+++ server.js
@@ -1,17 +1,38 @@
 const http = require("node:http");
+const fs = require("node:fs/promises");
 
 const PORT = 9000; 
 const ADDR = "localhost";
 
 const server = http.createServer();
 
-server.on("request", (req, res) => {
+server.on("request", async(req, res) => {
 
     if(req.url === "/" && req.method === "GET"){
 
         res.setHeader("Content-Type", "text/html");
+        const file = await fs.open("./public/index.html", "r");
+        const ReadStream = file.createReadStream();     // never write directly, always use streams
         
+        ReadStream.pipe(res);
+    };
 
+    if(req.url === "/styles.css" && req.method === "GET"){
+
+        res.setHeader("Content-Type", "text/css");
+        const file = await fs.open("./public/styles.css", "r");
+        const ReadStream = file.createReadStream();     // never write directly, always use streams
+        
+        ReadStream.pipe(res);
+    };
+
+    if(req.url === "/scripts.js" && req.method === "GET"){
+
+        res.setHeader("Content-Type", "text/javascript");
+        const file = await fs.open("./public/scripts.js", "r");
+        const ReadStream = file.createReadStream();     // never write directly, always use streams
+        
+        ReadStream.pipe(res);
     };
 
 });
